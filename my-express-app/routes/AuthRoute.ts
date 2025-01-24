1164d64ab5f593ebf489b27d65cef98ff4426947
Akash <akashjune11990@gmail.com>
2025-01-24 17:19:04 +0530

    complete working Authorization

--- my-express-app/routes/AuthRoute.ts
+++ my-express-app/routes/AuthRoute.ts
@@ -4,5 +4,7 @@ import * as GetAuthDetails from '../controllers/GetAuthDetails'
 const router = Router();
 
 router.post('/auth', GetAuthDetails.createSession);
+router.delete('/auth/:user_id', GetAuthDetails.deleteUser);
+router.put('/auth/:user_id', GetAuthDetails.updateUser);
 
-export default router;
+export default router;  
